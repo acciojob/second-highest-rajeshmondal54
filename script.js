2d133a9e6d6fd75afa082a0ea79fafe866b97ba4
rajeshmondal54 <rm4223536@gmail.com>
2024-07-21 01:02:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+  if (arr.length < 2) {
+    return -Infinity;
+  }
+
+  let highest = -Infinity;
+  let secondHighest = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > highest) {
+      secondHighest = highest;
+      highest = arr[i];
+    } else if (arr[i] > secondHighest && arr[i] !== highest) {
+      secondHighest = arr[i];
+    }
+  }
+
+  return secondHighest === -Infinity ? -Infinity : secondHighest;
+}
+
+module.exports = secondHighest;
